7ae54ee9adebd34b5e525a992083d7c1cf28fdaf
pnb <pnigelb@gmail.com>
2022-04-28 09:27:03 -0500

    Cache submitted papers list
    This was pretty slow to calculate because it parses the HTML of every submission to find the title. Now the info is cached in a JSON file per paper, which still requires reading a lot of files but not large ones.

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 import { JSDOM } from 'jsdom';
 
 
-export let router = Router();
+export const router = Router();
 
 router.docs_dir = false;
 
@@ -15,51 +15,58 @@ router.get('/submitted-papers', (req, res) => {
     const paper_info = {};
     fs.readdirSync(router.docs_dir, {withFileTypes: true}).forEach((entry) => {
         if (entry.isDirectory()) {
-            let submitted_time = 'error';
-            try {
-                const dir_stats = fs.statSync(path.join(router.docs_dir, entry.name));
-                submitted_time = Math.round(dir_stats.birthtimeMs);
-            } catch (_) {}
-
-            let source_ext = '.docx';
-            if (fs.existsSync(path.join(router.docs_dir, entry.name, entry.name + '.zip'))) {
-                source_ext = '.zip'
-            }
-
-            try {
-                const html = fs.readFileSync(path.join(router.docs_dir, entry.name, 'index.html'));
-                const doc = new JSDOM(html).window.document;
-                const titles = doc.getElementsByClassName('Paper-Title');
-                if (titles.length > 0) {
-                    const paper_title = titles.item(0).textContent;
-                    // Make key sortable by title, then submitted time
-                    paper_info[paper_title + submitted_time + entry.name] = {
-                        id: entry.name,
-                        source_ext: source_ext,
-                        time: submitted_time,
-                        title: paper_title,
-                    }
-                } else {
-                    paper_info[submitted_time + entry.name] = {
-                        id: entry.name,
-                        source_ext: source_ext,
-                        time: submitted_time,
-                        title: '*No title!',
-                    }
-                }
-            } catch (err) {
-                if (err.code == 'ENOENT') {
-                    paper_info[submitted_time + entry.name] = {
-                        id: entry.name,
-                        source_ext: source_ext,
-                        time: submitted_time,
-                        title: '**No index.html (conversion probably failed)',
-                    }
-                } else {
-                    console.error('Unexpected error!', err);
-                }
-            }
+            const cur_paper = read_paper_data(entry.name);
+            // Make key sortable by title, then submitted time
+            paper_info[cur_paper.title + cur_paper.time + cur_paper.id] = cur_paper;
         }
     });
     res.render('admin/submitted', {papers: paper_info});
 });
+
+
+function read_paper_data(dir) {
+    try {  // Check cached result first
+        const jstr = fs.readFileSync(path.join(router.docs_dir, dir, 'info_cache.json'), 'utf8');
+        return JSON.parse(jstr);
+    } catch (_) {}
+
+    const cur_paper = {
+        id: dir,
+        time: 'error',
+        source_ext: '.docx',
+        title: '***Unexpected error',
+    };
+
+    if (fs.existsSync(path.join(router.docs_dir, dir, dir + '.zip'))) {
+        cur_paper.source_ext = '.zip';
+    }
+
+    try {
+        const dir_stats = fs.statSync(path.join(router.docs_dir, dir));
+        cur_paper.time = Math.round(dir_stats.birthtimeMs);
+    } catch (_) {}
+
+    try {
+        const html = fs.readFileSync(path.join(router.docs_dir, dir, 'index.html'));
+        const doc = new JSDOM(html).window.document;
+        const titles = doc.getElementsByClassName('Paper-Title');
+        if (titles.length > 0) {
+            cur_paper.title = titles.item(0).textContent;
+        } else {
+            cur_paper.title = '*No title!';
+        }
+    } catch (err) {
+        if (err.code == 'ENOENT') {
+            cur_paper.title = '**No index.html (conversion probably failed)';
+        }
+    }
+
+    try {  // Cache for next time
+        fs.writeFileSync(path.join(router.docs_dir, dir, 'info_cache.json'),
+            JSON.stringify(cur_paper), 'utf8');
+    } catch (err) {
+        console.error('Unexpected error:', err);
+    }
+
+    return cur_paper;
+}
