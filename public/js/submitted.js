393759ec67914d7d54e971a70a3efeca5fd72931
Nigel Bosch <pnigelb@gmail.com>
2025-01-12 17:36:10 -0600

    StandardJS admin papers page
    I was going to make it sortable too but then realized that would mess up paper title merging and it is already sorted well enough

--- public/js/submitted.js
+++ public/js/submitted.js
@@ -1,23 +1,24 @@
-function hide_previous_attempts() {
-    const titles = document.getElementsByClassName('title');
-    for (let i = 0; i < titles.length; ++i) {
-        if (i > 0 && titles.item(i).textContent == titles.item(i - 1).textContent) {
-            // Previous row should be marked hidden as hidden since rows with the same title are in
-            // increasing order by time.
-            titles.item(i - 1).parentElement.classList.add('hidden');
-        }
+function hidePreviousAttempts () {
+  const titles = document.getElementsByClassName('title')
+  for (let i = 0; i < titles.length; ++i) {
+    if (i > 0 && titles.item(i).textContent === titles.item(i - 1).textContent) {
+      // Previous row should be marked hidden as hidden since rows with the same title
+      // are in increasing order by time
+      titles.item(i - 1).parentElement.classList.add('hidden')
     }
+  }
 }
 
 if (document.getElementById('chk-only-latest').checked) {
-    // Page was refreshed or something and the checkbox is already checked
-    hide_previous_attempts();
+  // Page was refreshed or something and the checkbox is already checked
+  hidePreviousAttempts()
 }
 
 document.getElementById('chk-only-latest').onchange = (e) => {
-    if (e.target.checked) {
-        hide_previous_attempts();
-    } else {
-        document.querySelectorAll('tr.hidden').forEach((row) => row.classList.remove('hidden'));
-    }
+  if (e.target.checked) {
+    hidePreviousAttempts()
+  } else {
+    document.querySelectorAll('tr.hidden')
+      .forEach((row) => row.classList.remove('hidden'))
+  }
 }
